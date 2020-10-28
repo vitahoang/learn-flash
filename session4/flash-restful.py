b04c3b074dfb571c574132d42f61c62b4c07b5e4
vitahoang <hunghoang.vita@gmail.com>
2020-10-28 15:19:14 +0700

    improve error control

--- session4/flash-restful.py
+++ session4/flash-restful.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
+app.secret_key = 'vita'
 api = Api(app)
 
 items = []
@@ -14,12 +15,12 @@ class HelloWorld(Resource):
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return {'item': None}, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        return {'item': None}, 200 if item else 404
 
     def post(self, name):
+        if next(filter(lambda x: x['name'] == name, items), None) is not None:
+            return {'Message': "The item '{}' is already exist".format(name)}, 400
         data = request.get_json()
         item = {'name': name, 'price': data['price']}
         items.append(item)
