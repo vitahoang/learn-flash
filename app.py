2f920c36eb4e944d88c8eaa4155701c361a182ec
vitahoang <hunghoang.vita@gmail.com>
2020-11-13 17:31:01 +0700

    create run file to declare db

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -14,11 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
 jwt = JWT(app, authenticate, identity)  # /auth
-
-
-@app.before_first_request
-def create_db():
-    db.create_all()
 
 
 class HelloWorld(Resource):
@@ -34,5 +28,4 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000)
